639531fdbf99c6830e2b9b6aaae185c9a8966f01
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-07-02 09:52:30 -0300

    added fetchCharacter & fetchDeath

--- BBQuotes/FetchService.swift
+++ BBQuotes/FetchService.swift
@@ -34,4 +34,45 @@ struct FetchService {
         // Return quote
         return quote
     }
+    
+    func fetchCharacter(_ name: String) async throws -> Char{
+        let characterURL = baseURL.appending(path: "characters")
+        let fetchURL = characterURL.appending(queryItems: [URLQueryItem(name: "name", value: name)])
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+    
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let characters = try decoder.decode([Char].self, from: data)
+        
+        return characters[0]
+    }
+    
+    func fetchDeath(for character: String) async throws -> Death? {
+        let fetchURL = baseURL.appending(path: "deaths")
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let deaths = try decoder.decode([Death].self, from: data)
+        
+        for death in deaths {
+            if death.character == character {
+                return death
+            }
+        }
+        
+        return nil
+    }
 }
