9de5a838961cb6985c8de1eb40c2990365e6a260
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-07-04 18:37:05 -0300

    Init from decoder

--- BBQuotes/FetchService.swift
+++ BBQuotes/FetchService.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 struct FetchService {
-    enum FetchError: Error {
+    private enum FetchError: Error {
         case badResponse
     }
     
-    let baseURL = URL(string: "https://breaking-bad-api-six.vercel.app/api")!
+    private let baseURL = URL(string: "https://breaking-bad-api-six.vercel.app/api")!
     
     //https://breaking-bad-api-six.vercel.app/api/quotes/random?production=Breaking+Bad
     func fetchQuote(from show: String) async throws -> Quote {
---
+++ BBQuotes/ViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  ViewModel.swift
+//  BBQuotes
+//
+//  Created by Thiago Ogawa on 04/07/25.
+//
+
+import Foundation
+
+@Observable
+@MainActor
+class ViewModel {
+    enum FetchStatus {
+        case notStarted
+        case fetching
+        case success
+        case failed(error: Error)
+    }
+    
+    private(set) var status: FetchStatus = .notStarted
+    
+    private let fetcher = FetchService()
+    
+    var quote: Quote
+    var character: Char
+    
+    init() {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let quoteData = try! Data(contentsOf: Bundle.main.url(forResource: "samplequote", withExtension: "json")!)
+        quote = try! decoder.decode(Quote.self, from: quoteData)
+        
+        let characterData = try! Data(contentsOf: Bundle.main.url(forResource: "samplecharacter", withExtension: "json")!)
+        character = try! decoder.decode(Char.self, from: characterData)
+    }
+}
